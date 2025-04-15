9838c6a6048904ad89ae01cc01ccd2ce55cf7246
Sergio Vegas Alonso <sergiovegas@capitole-consulting.com>
2025-04-15 23:21:35 +0200

    console app in beta

---
+++ TodoLists/src/ConsoleApp/DependencyInjection.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp;
+
+public static class DependencyInjection
+{
+
+}
--- TodoLists/src/ConsoleApp/Program.cs
+++ TodoLists/src/ConsoleApp/Program.cs
@@ -1,22 +1,13 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TodoLists.Application.UseCases.AddItem;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Ensure the correct environment and configuration file are loaded
-builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
-
-// Debugging: Print the connection string
-Console.WriteLine(builder.Configuration.GetConnectionString("BeyondTestDb"));
-
 // Add services to the container.
 builder.AddApplicationServices();
 builder.AddInfrastructureServices();
-
-//builder.Services.AddMediatR(typeof(AddItemCommand).Assembly);
 
 var app = builder.Build();
 
