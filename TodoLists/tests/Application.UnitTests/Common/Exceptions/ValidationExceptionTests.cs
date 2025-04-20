d007fd3d46ff46b95001ceab196b86e15475b49f
Sergio Vegas Alonso <sergiovegas@capitole-consulting.com>
2025-04-21 01:40:51 +0200

    tests added 2

--- TodoLists/tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
+++ TodoLists/tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
@@ -20,13 +20,13 @@ public class ValidationExceptionTests
     {
         var failures = new List<ValidationFailure>
             {
-                new ValidationFailure("Age", "must be over 18"),
+                new ValidationFailure("Percent", "must be between 0 and 100 inclusive"),
             };
 
         var actual = new ValidationException(failures).Errors;
 
-        actual.Keys.Should().BeEquivalentTo(new string[] { "Age" });
-        actual["Age"].Should().BeEquivalentTo(new string[] { "must be over 18" });
+        actual.Keys.Should().BeEquivalentTo(new string[] { "Percent" });
+        actual["Percent"].Should().BeEquivalentTo(new string[] { "must be between 0 and 100 inclusive" });
     }
 
     [Test]
@@ -34,8 +34,8 @@ public class ValidationExceptionTests
     {
         var failures = new List<ValidationFailure>
             {
-                new ValidationFailure("Age", "must be 18 or older"),
-                new ValidationFailure("Age", "must be 25 or younger"),
+                new ValidationFailure("Percent", "must be between 0 and 100 inclusive"),
+                new ValidationFailure("Percent", "must be not exceed 100"),
                 new ValidationFailure("Password", "must contain at least 8 characters"),
                 new ValidationFailure("Password", "must contain a digit"),
                 new ValidationFailure("Password", "must contain upper case letter"),
@@ -44,12 +44,12 @@ public class ValidationExceptionTests
 
         var actual = new ValidationException(failures).Errors;
 
-        actual.Keys.Should().BeEquivalentTo(new string[] { "Password", "Age" });
+        actual.Keys.Should().BeEquivalentTo(new string[] { "Password", "Percent" });
 
-        actual["Age"].Should().BeEquivalentTo(new string[]
+        actual["Percent"].Should().BeEquivalentTo(new string[]
         {
-                "must be 25 or younger",
-                "must be 18 or older",
+                "must be between 0 and 100 inclusive",
+                "must be not exceed 100",
         });
 
         actual["Password"].Should().BeEquivalentTo(new string[]
